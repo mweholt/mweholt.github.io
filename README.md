7716286a9f2da083d4bcb3d71dbb4623b24d5206
Mitchell Weholt <mitch.weholt@gmail.com>
2016-07-26 16:34:35 -0700

    Updated variable names

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # IG is DOWN?!
 
 When Instagram goes down people lose their minds. This site is meant to give people a few ideas on how to fill the void.
-
--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var myArray = ["Read a book by the pool", "Go on a hike", "Drink wine in the bath", "Throw up your letters in a bathing suit",
+var itemArray = ["Read a book by the pool", "Go on a hike", "Drink wine in the bath", "Throw up your letters in a bathing suit",
   "Hold a college flag", "Hot dogs are legs", "Make latte art", "Eat an acai bowl", "Go to a concert", "Talk about 'your best friends in the whole wide world'",
   "Talk about how you can't wait for the weekend", "Go camping", "Drive a convertable", "Draw mediocre lettering in a journal", "Go to the gym", "Duck face", "Do yoga",
   "Look at the skyline", "Go mountain biking", "Literally anything in a bikini", "Look at a foggy forest", "Use the snapchat dog filter", "Pose like a bodybuilder", "Buy a puppy",
@@ -9,8 +9,8 @@ var currentIndex = 0;
 print.innerHTML = "Click for something else to do.";
 
 function nextElement() {
-  if (currentIndex == myArray.length){
-    shuffle(myArray);
+  if (currentIndex == itemArray.length){
+    shuffle(itemArray);
     currentIndex = 0;
   }
 
