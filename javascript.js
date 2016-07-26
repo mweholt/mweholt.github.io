becf421c07c4ffee268a23a3668b8c64ff1c3bfd
Mitchell Weholt <mitch.weholt@gmail.com>
2016-07-26 14:44:54 -0700

    Styles

--- javascript.js
+++ javascript.js
@@ -2,12 +2,11 @@ var myArray = ["Read a book by the pool", "Go on a hike", "Drink wine in the bat
   "Hold a college flag", "Hot dogs are legs", "Make latte art", "Eat an acai bowl", "Go to a concert", "Talk about 'your best friends in the whole wide world'",
   "Talk about how you can't wait for the weekend", "Go camping", "Drive a convertable", "Draw mediocre lettering in a journal", "Go to the gym", "Duck face", "Do yoga",
   "Look at the skyline", "Go mountain biking", "Literally anything in a bikini", "Look at a foggy forest", "Use the snapchat dog filter", "Pose like a bodybuilder", "Buy a puppy",
-  "Bake cookies", "Prank your friends", "Decorate your room"];
+  "Bake cookies", "Prank your friends", "Decorate your room", "Buy a Sony A7s", "Go to Snoqualmie Falls", "Become a model", "Listen to Drake", "Offer 15% off watches"];
 var print = document.getElementById('print');
-var myIndex = 1;
 
 print.innerHTML = "Click for something else to do.";
 
 function nextElement() {
-   print.innerHTML = myArray[myIndex++%myArray.length];
+   print.innerHTML = myArray[Math.floor((Math.random() * myArray.length))];
 };
