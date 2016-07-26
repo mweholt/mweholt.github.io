06ea53d0e2def702ed34dafbf84b97312341683e
Mitchell Weholt <mitch.weholt@gmail.com>
2016-07-26 13:51:43 -0700

    More beautifying

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 var myArray = ["Read a book by the pool", "Go on a hike", "Drink wine in the bath", "Throw up your letters in a bathing suit",
   "Hold a college flag", "Hot dogs are legs", "Make latte art", "Eat an acai bowl", "Go to a concert", "Talk about 'your best friends in the whole wide world'",
   "Talk about how you can't wait for the weekend", "Go camping", "Drive a convertable", "Draw mediocre lettering in a journal", "Go to the gym", "Duck face", "Do yoga",
-  "Look at the skyline", "Go mountain biking", "Literally anything in a bikini", "Look at a foggy forest", "Use the snapchat dog filter", "Pose like a bodybuilder", "Buy a puppy"];
+  "Look at the skyline", "Go mountain biking", "Literally anything in a bikini", "Look at a foggy forest", "Use the snapchat dog filter", "Pose like a bodybuilder", "Buy a puppy",
+  "Bake cookies", "Prank your friends", "Decorate your room"];
 var print = document.getElementById('print');
 var myIndex = 1;
 
